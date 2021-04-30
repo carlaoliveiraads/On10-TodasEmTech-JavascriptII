12411b9eb95bb6967f4375e9ba5d42ad03f2ca11
Carla Oliveira <carlaoliveira.ads@gmail.com>
2021-04-29 21:08:28 -0300

    Codigo rodando

---
+++ Exercicios/10. array.map()/script2.js
@@ -0,0 +1,26 @@
+const winners = [ 
+    {
+        nome: 'Equipe Maravilinda',
+        pais: 'Canadá',
+     },
+    {
+        nome: 'Liga da Justiça',
+        pais: 'EUA',
+    },
+    {
+        nome: 'Mega Grupo',
+        pais: 'Brasil',
+    }
+];
+
+// const vencedores = winners.map((equipe) => {
+//   return equipe.nome;
+// })
+
+// console.log(vencedores);
+
+const paises = winners.map(function(local){
+    return local.pais
+});
+
+console.log(paises)
---
+++ Exercicios/script.js
@@ -0,0 +1,6 @@
+const listaDeCompras = ["Biquini", "Equipamento" , "Joias" , "Roupas"];
+
+for (let itens = 0; itens < listaDeCompras.length; itens++){
+    console.log(listaDeCompras);
+    
+}
